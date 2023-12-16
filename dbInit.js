e2d3881cce22d10d86893cf6b4195d84dda9724c
baz <bazzadobs@live.co.uk>
2023-12-16 20:38:58 +0000

    Create dbInit

---
+++ dbInit.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'database.sqlite',
+});
+
+const db = require('./models/games.js')(sequelize, Sequelize.DataTypes);
+require('./models/users.js')(sequelize, Sequelize.DataTypes);
+require('models/beatenGames.js')(sequelize, Sequelize.DataTypes);
+
+const force = process.argv.includes('--force') || process.argv.includes('-f');
+
+sequelize.sync({ force }).then(async () => {
+    // await Promise.all();
+    console.log('Database synced');
+    sequelize.close();
+}).catch(console.error);
