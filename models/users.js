78cd678f6f2f9ff485605f565082d95495fbbbf0
baz <bazzadobs@live.co.uk>
2023-12-16 20:38:00 +0000

    Create Users sequelize module

---
+++ models/users.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('users', {
+        user_id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    }, {
+        timestamps: false,
+    });
+};
