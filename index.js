2e4e68a82b8ffb3e70a0d2e100a09e4ff5decbaa
baz <bazzadobs@live.co.uk>
2023-12-18 21:55:20 +0000

    Fix requires

--- index.js
+++ index.js
@@ -50,30 +50,9 @@ for (const file of eventFiles) {
 
 client.login(process.env.token);
 
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('database', 'user', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	storage: 'database.sqlite',
-});
-
-const Tags = sequelize.define('tags', {
-	name: {
-		type: Sequelize.STRING,
-		unique: true,
-	},
-	description: Sequelize.TEXT,
-	username: Sequelize.STRING,
-	usage_count: {
-		type: Sequelize.INTEGER,
-		defaultValue: 0,
-		allowNull: false,
-	},
-});
-
 client.once(Events.ClientReady, () => {
-	Tags.sync({ force: true });
 	console.log(`Logged in as ${client.user.tag}!`);
 });
+
+require('sequelize');
+require('./dbObjects.js');
