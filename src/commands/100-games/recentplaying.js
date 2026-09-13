7523bdf5a3f28e4620a4420c8b0dbd8e255a2705
baz <bazzadobs@live.co.uk>
2026-09-13 23:12:46 +0100

    Update recentplaying logic

--- src/commands/100-games/recentplaying.js
+++ src/commands/100-games/recentplaying.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { getCoverURL, getGameJson } = require('../../helpers/igdb.js');
-const { getUserRegistration, getBeatenGameCount, getPlanningGameCount, getPlayingGameCount, getRecentPlayingGameEntry } = require('../../helpers/database');
+const { getUserRegistration, getBeatenGameCount, getPlanningGameCount, getPlayingGameCount, getRecentPlayingLoggedGameEntry, checkGameStorageId } = require('../../helpers/database');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,47 +10,45 @@ module.exports = {
     async execute(interaction) {
         await interaction.deferReply();
 
-        let user = interaction.user;
-        const userOption = interaction.options.getUser('user');
-
-        if (userOption) {
-            user = userOption;
-        }
+        const user = interaction.options.getUser('user') ?? interaction.user;
 
         const userDatabaseEntry = await getUserRegistration(user);
         if (!userDatabaseEntry) return interaction.editReply({ content: `Issue checking registration with "${interaction.user.username}".`, ephemeral: true });
 
-        const loggedGameEntry = await getRecentPlayingGameEntry(userDatabaseEntry);
+        const loggedGameEntry = await getRecentPlayingLoggedGameEntry(userDatabaseEntry);
         if (!loggedGameEntry) return interaction.editReply({ content: 'No game found.', ephemeral: true });
 
-        const gameDatabaseEntry = await getRecentPlayingGameEntry(userDatabaseEntry);
+        const gameDatabaseEntry = await checkGameStorageId(loggedGameEntry.gameId);
         if (!gameDatabaseEntry) return interaction.editReply({ content: 'No game found.', ephemeral: true });
 
         const body = `where id = ${ gameDatabaseEntry.igdb_id }; fields *;`;
         const res = await getGameJson(body);
-        if (!res) return interaction.editReply({ content: 'No game found.', ephemeral: true });
+        if (!res?.length) return interaction.editReply({ content: 'No game found on igdb.', ephemeral: true });
+
         const game = res[0];
 
-        const beatNum = await getBeatenGameCount(userDatabaseEntry);
-        const planNum = await getPlanningGameCount(userDatabaseEntry);
-        const playNum = await getPlayingGameCount(userDatabaseEntry);
+        const [beatNum, planNum, playNum] = await Promise.all([
+            getBeatenGameCount(userDatabaseEntry),
+            getPlanningGameCount(userDatabaseEntry),
+            getPlayingGameCount(userDatabaseEntry),
+        ]);
 
-        const embed = new EmbedBuilder()
-            .setColor(0x00C921)
-            .setAuthor({ name: `${user.displayName}'s most recent planned game`, iconURL: user.avatarURL() })
-            .setTitle(game.name)
-            .setURL(game.url)
-            .setFooter({ text: 'The Ochulus • 100 Games Challenge', iconURL: interaction.client.user.avatarURL() })
-            .setTimestamp(loggedGameEntry.statusLastChanged);
+        const embed = new EmbedBuilder();
+        embed.setColor(0x00C921);
+        embed.setAuthor({ name: `${user.displayName}'s most recent playing game`, iconURL: user.avatarURL() });
+        embed.setTitle(game.name);
+        embed.setURL(game.url);
+        embed.setFooter({ text: 'The Ochulus • 100 Games Challenge', iconURL: interaction.client.user.avatarURL() });
+        embed.setTimestamp(loggedGameEntry.statusLastChanged);
 
         if (game.cover) {
             const coverUrl = await getCoverURL(game.cover);
             embed.setThumbnail(`${coverUrl}`);
         }
 
-        embed.addFields({ name: 'Planned', value: `${planNum} game(s)`, inline: true });
-        embed.addFields({ name: 'Now Playing', value: `${playNum} game(s)`, inline: true });
-        embed.addFields({ name: 'Beaten', value: `${beatNum}/100 (${100 - beatNum} game(s) remaining)`, inline: true });
+        embed.addFields({ name: 'Planned', value: `${planNum} game${planNum !== 1 ? 's' : ''}`, inline: true });
+        embed.addFields({ name: 'Now Playing', value: `${playNum} game${playNum !== 1 ? 's' : ''}`, inline: true });
+        embed.addFields({ name: 'Beaten', value: `${beatNum} game${beatNum !== 1 ? 's' : ''} *(${100 - beatNum} game${100 - beatNum !== 1 ? 's' : ''} remaining)*`, inline: true });
 
         return interaction.editReply({ embeds: [embed] });
     },
